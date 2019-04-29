47aed4a8f4fa5b482793eae21cebdaf05aebe0b5
huhen <kinetic@bk.ru>
2019-04-30 02:45:45 +0300

    Fix std types

--- inc/stm8s.h
+++ inc/stm8s.h
@@ -207,6 +207,7 @@
 #define     __O     volatile         /*!< defines 'write only' permissions    */
 #define     __IO    volatile         /*!< defines 'read / write' permissions  */
 
+#ifndef _STDINT
 /*!< Signed integer types  */
 typedef   signed char     int8_t;
 typedef   signed short    int16_t;
@@ -216,6 +217,7 @@ typedef   signed long     int32_t;
 typedef unsigned char     uint8_t;
 typedef unsigned short    uint16_t;
 typedef unsigned long     uint32_t;
+#endif /* _STDINT */
 
 /*!< STM8 Standard Peripheral Library old types (maintained for legacy purpose) */
 
@@ -227,8 +229,9 @@ typedef uint32_t  u32;
 typedef uint16_t u16;
 typedef uint8_t  u8;
 
-
+#ifndef __cplusplus
 typedef enum {FALSE = 0, TRUE = !FALSE} bool;
+#endif /* !__cplusplus */
 
 typedef enum {RESET = 0, SET = !RESET} FlagStatus, ITStatus, BitStatus, BitAction;
 
